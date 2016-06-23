00fd8a0b88d686412ba1f67a7551e0503714efea
Stephan Schuler <stephan.schuler@netlogix.de>
2016-06-23 22:53:00 +0200

    Export all files on root level

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./service/'));
-__export(require('./domain/model/'));
-__export(require('./domain/repository/'));
+var consumer_backend_1 = require('./service/consumer-backend');
+exports.ConsumerBackend = consumer_backend_1.ConsumerBackend;
+var resource_proxy_1 = require('./domain/model/resource-proxy');
+exports.ResourceProxy = resource_proxy_1.ResourceProxy;
+var type_1 = require('./domain/model/type');
+exports.Type = type_1.Type;
+var property_1 = require('./domain/model/property');
+exports.Property = property_1.Property;
+var uri_1 = require('./domain/model/uri');
+exports.Uri = uri_1.Uri;
+var resource_proxy_repository_1 = require('./domain/repository/resource-proxy-repository');
+exports.ResourceProxyRepository = resource_proxy_repository_1.ResourceProxyRepository;
 //# sourceMappingURL=index.js.map
